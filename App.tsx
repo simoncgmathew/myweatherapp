b6225f09918495181ccb9c8d20977a25a525978c
academy-01 <academy-01@academy-01s-MacBook-Pro.local>
2023-03-10 11:56:16 +0800

    M2L1 - JSX

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+interface HelloWorldProps {
+  shouldRenderWorld: boolean;
+}
+
+function HelloWorld(props: HelloWorldProps) {
+  return (
+    <View>
+      <Text>Hello</Text>
+      {props.shouldRenderWorld && <Text>World</Text>}
+    </View>
+  );
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <HelloWorld shouldRenderWorld />
       <StatusBar style="auto" />
     </View>
   );
